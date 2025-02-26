a85ec290cf824b92963a29eec13717e3baea1a1a
Jefferson Santos <xjeehz@gmail.com>
2025-02-26 14:58:20 -0300

    fix: editorconfig fixes

--- src/components/sortableList/index.tsx
+++ src/components/sortableList/index.tsx
@@ -2,30 +2,26 @@ import React from "react";
 import { SortableElement, SortableContainer } from "react-sortable-hoc";
 import { arrayMoveImmutable } from "array-move";
 
-const SortableItem = SortableElement(
-  ({ value, i }: { value: string; i: number }) => {
-    return (
-      // <li>{value}</li>
-      <div className="item">
-        <div className="order">{i + 1}</div>
-        <div className="description">{value}</div>
-        <div className="action">
-          <div className="remove" onClick={() => console.log("remove")}>
-            X
-          </div>
+const SortableItem = SortableElement(({ value, i }: { value: string; i: number }) => {
+  return (
+    // <li>{value}</li>
+    <div className="item">
+      <div className="order">{i + 1}</div>
+      <div className="description">{value}</div>
+      <div className="action">
+        <div className="remove" onClick={() => console.log("remove")}>
+          X
         </div>
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
 
 const SortableList = SortableContainer(({ items }: { items: any }) => {
   return (
     <div className="list">
       {items?.map((item: any, i: number) => {
-        return (
-          <SortableItem key={`item-${item}`} index={i} value={item} i={i} />
-        );
+        return <SortableItem key={`item-${item}`} index={i} value={item} i={i} />;
       })}
     </div>
   );
