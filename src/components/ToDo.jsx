81896f5a47490fab7774810e6e1600e9c0f1e853
Jefferson Santos <xjeehz@gmail.com>
2025-03-09 13:14:04 -0300

    feat: reorder

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./ToDo";
 import css from "./ToDo.module.scss";
 
-import { ArrowDropUp, ArrowDropDown, Clear } from "@mui/icons-material";
+import { Clear } from "@mui/icons-material";
 import store from "store2";
 import { Icon } from "./Icon";
+import { Draggable } from "react-drag-reorder";
 
 const Todo = () => {
   const [list, setList] = useState(store("todo"));
@@ -13,6 +14,8 @@ const Todo = () => {
   //   { id: "56e87405-d9c4-4b65-a4b2-4d112fae83ac", order: 1, description: "test2", finished: false },
   // ]);
   const [todo, setTodo] = useState({ order: list.length, description: "", finished: false });
+
+  const [isEditing, setIsEditing] = useState(false);
 
   const categories = [
     {
@@ -29,6 +32,8 @@ const Todo = () => {
     },
   ];
 
+  const listRef = useRef(null);
+
   const onChange = ({ target: { value } }) => setTodo((prevState) => ({ ...prevState, description: value }));
 
   const onToggleFinished = async ({ target: { checked } }, index) => {
@@ -38,7 +43,7 @@ const Todo = () => {
     store("todo", updatedList);
   };
 
-  const add = async () => {
+  const save = async () => {
     const newTodo = {
       id: crypto.randomUUID(),
       order: list.length,
@@ -51,13 +56,22 @@ const Todo = () => {
     store("todo", updatedList);
   };
 
-  const reorder = (direction, index) => {
+  const drag = (currentPos, newPos) => {
     const updatedList = [...list];
-    const item = updatedList[index];
-    updatedList[index] = updatedList[index + (direction === "up" ? -1 : 1)];
-    updatedList[index + (direction === "up" ? -1 : 1)] = item;
+    const item = updatedList[currentPos];
+    updatedList.splice(currentPos, 1);
+    updatedList.splice(newPos, 0, item);
     setList(updatedList);
     store("todo", updatedList);
+  };
+
+  const edit = (index) => {
+    setIsEditing(true);
+    setTodo({
+      order: list[index].order,
+      description: list[index].description,
+      finished: list[index].finished,
+    });
   };
 
   const remove = (index) => {
@@ -68,7 +82,7 @@ const Todo = () => {
 
   return (
     <div className={css.wrapper}>
-      <div className={css.tabs}>
+      <div className={css.categories}>
         <select>
           {categories.map((item) => {
             const { id, name } = item;
@@ -80,36 +94,38 @@ const Todo = () => {
           })}
         </select>
       </div>
+
       <div className={css.todo}>
-        <div className={css.list}>
-          <div className={css.new}>
-            <>
-              <div className={css.order}></div>
-              <div className={css.description}>
-                <input type="text" placeholder="Add a new item" value={todo.description} onChange={onChange} />
-              </div>
-              <button className={css.add} onClick={add}>
-                {<Icon name={"Add"} color="white" />}
-              </button>
-            </>
-          </div>
-          {list?.map((item, i) => {
-            const { order, description, finished } = item;
-            return (
-              <div className={css.item} key={i}>
-                <div className={css.finished}>
-                  <input type="checkbox" checked={finished} onChange={(e) => onToggleFinished(e, i)} />
+        <div className={css.form}>
+          <>
+            <div className={css.order}></div>
+            <div className={css.description}>
+              <input type="text" placeholder="Add a new item" value={todo.description} onChange={onChange} />
+            </div>
+            <button className={css.add} onClick={save}>
+              {<Icon name={isEditing ? "Edit" : "Add"} color="white" />}
+            </button>
+          </>
+        </div>
+
+        <div className={css.list} ref={listRef}>
+          <Draggable onPosChange={drag}>
+            {list?.map((item, i) => {
+              const { order, description, finished } = item;
+              return (
+                <div className={css.item} key={i} onClick={() => edit(i)}>
+                  <div className={css.finished}>
+                    <input type="checkbox" checked={finished} onChange={(e) => onToggleFinished(e, i)} />
+                  </div>
+                  <div className={css.order}>{order + 1}</div>
+                  <div className={css.description}>{description}</div>
+                  <div className={css.action}>
+                    <Clear onClick={() => remove(i)} />
+                  </div>
                 </div>
-                <div className={css.order}>{order + 1}</div>
-                <div className={css.description}>{description}</div>
-                <div className={css.action}>
-                  {i > 0 && <ArrowDropUp onClick={() => reorder("up", i)} />}
-                  {i < list.length - 1 && <ArrowDropDown onClick={() => reorder("down", i)} />}
-                  <Clear onClick={() => remove(i)} />
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </Draggable>
         </div>
       </div>
     </div>
