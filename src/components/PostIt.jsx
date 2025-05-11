908eba25e1c73bb4e8a492c5693b2d734421f8d9
Jefferson Santos <xjeehz@gmail.com>
2025-05-11 19:37:15 -0300

    feat: Resizable PostIt

--- src/components/PostIt.jsx
+++ src/components/PostIt.jsx
@@ -6,9 +6,20 @@ import store from "store2";
 
 export const PostIt = () => {
   const storeIndex = "postIt-daily";
-  const [postIt, setPostIt] = useState(store.get(storeIndex));
+  const [postIt, setPostIt] = useState(
+    store.get(storeIndex) || {
+      x: "0px",
+      y: "50px",
+      width: "300px",
+      height: "200px",
+      isMinimized: false,
+      isClosed: false,
+      text: "",
+    }
+  );
 
   const ref = useRef(null);
+  const offset = useRef({ x: 0, y: 0 });
 
   useEffect(() => {
     store.set(storeIndex, postIt);
@@ -28,8 +39,6 @@ export const PostIt = () => {
     setPostIt((prev) => ({ ...prev, text: target.value }));
   };
 
-  const offset = useRef({ x: 0, y: 0 });
-
   const dragStart = (e) => {
     const rect = ref.current.getBoundingClientRect();
     offset.current.x = e.clientX - rect.left;
@@ -38,7 +47,8 @@ export const PostIt = () => {
     e.dataTransfer.setData("text/plain", "dragging");
 
     const img = new Image();
-    img.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEWZmZmILVbPAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=";
+    img.src =
+      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEWZmZmILVbPAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=";
     e.dataTransfer.setDragImage(img, 0, 0);
   };
 
@@ -60,10 +70,20 @@ export const PostIt = () => {
     setPostIt((prev) => ({ ...prev, x: newX, y: newY }));
   };
 
-  const { x, y, isMinimized, isClosed, text } = postIt;
+  const { x, y, isMinimized, isClosed, text, width, height } = postIt;
 
   return (
-    <div ref={ref} style={{ left: x, top: y }} className={classNames(css.wrapper, { [css.minimized]: isMinimized, [css.closed]: isClosed })} draggable="true" onDragStart={dragStart} onDrag={dragNote}>
+    <div
+      ref={ref}
+      style={{ left: x, top: y, width: width, height: height }}
+      className={classNames(css.wrapper, {
+        [css.minimized]: isMinimized,
+        [css.closed]: isClosed,
+      })}
+      draggable="true"
+      onDragStart={dragStart}
+      onDrag={dragNote}
+    >
       <div className={css.windowActions} onClick={toggleMinimize}>
         <Icon name={isMinimized ? "Crop32" : "Minimize"} color="black" />
         <Icon name="Close" color="red" onClick={() => close()} />
